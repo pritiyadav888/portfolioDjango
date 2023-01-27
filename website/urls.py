cf3e432128e7724cdab56108f1e5a3a7264584cb
Priti Yadav <pritiyadav888.com>
2023-01-27 15:10:36 -0800

    php corrected

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('portfolio-details.html', views.portfolio_details, name='portfolio_details'),
     path('blog-single.html', views.blog_single, name='blog_single'),
-    path('/forms/contact.php', views.contact_php, name='contact_php'),
+    path('forms/contact.php', views.contact_php, name='contact_php'),
 ]
