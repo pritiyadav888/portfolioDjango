0a92e688a782ac4c9c8c626af8098a3f6693ded1
Priti Yadav <pritiyadav888.com>
2023-01-30 15:10:39 -0800

    new vercel

--- basicImagetoText/settings.py
+++ basicImagetoText/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
