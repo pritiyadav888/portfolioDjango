1ccce7d0e7734748d7d47ccfb66dca085fa78402
Priti Yadav <pritiyadav888.com>
2023-01-29 18:42:29 -0800

    dependencies

--- basicImagetoText/settings.py
+++ basicImagetoText/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c=_&i@ufj7a3uq%7+@-ix23ditrazwqg!ddbf1@)z**sym^uop"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
