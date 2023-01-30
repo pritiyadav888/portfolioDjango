9a3560dfa1dbbf34060adcdb221d1ea5bd4f0bbf
Priti Yadav <pritiyadav888.com>
2023-01-29 20:38:15 -0800

    env

--- basicImagetoText/settings.py
+++ basicImagetoText/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c=_&i@ufj7a3uq%7+@-ix23ditrazwqg!ddbf1@)z**sym^uop"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
